68dbae19cb5f52f89835c17b12d61a16061f929d
shannonbnd <bundhoos29@gmail.com>
2026-03-25 19:57:06 +0100

    Adjust camera orbit and field of view values

--- src/app/Appareil3D.tsx
+++ src/app/Appareil3D.tsx
@@ -3,10 +3,10 @@ import { PROJECTS } from "../data/projects";
 
 type ProjectTicket = (typeof PROJECTS)[number];
 
-const DESKTOP_CAMERA_ORBIT = "-5deg 82deg 5.4m";
-const MOBILE_CAMERA_ORBIT = "-5deg 82deg 6.1m";
+const DESKTOP_CAMERA_ORBIT = "-5deg 82deg 5m";
+const MOBILE_CAMERA_ORBIT = "-5deg 82deg 5.6m";
 const BASE_CAMERA_TARGET = "-0.4m 0m 0m";
-const BASE_FOV = "32deg";
+const BASE_FOV = "28deg";
 
 type Appareil3DProps = {
   viewerRef: RefObject<any>;
