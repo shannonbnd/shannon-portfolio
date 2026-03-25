494f8c269f92f6824a49b32119da7dd74b178851
shannonbnd <bundhoos29@gmail.com>
2026-03-25 20:21:45 +0100

    Adjust top position for mobile view in Appareil3D

--- src/app/Appareil3D.tsx
+++ src/app/Appareil3D.tsx
@@ -59,7 +59,7 @@ export default function Appareil3D({
           background: "transparent",
           overflow: "visible",
           left: "50%",
-          top: isMobile ? "15%" : "30%",
+          top: isMobile ? "5%" : "30%",
           width: isMobile ? "420px" : "1080px",
           height: isMobile ? "700px" : "1920px",
           transform: isMobile ? "translateX(-50%)" : "translate(-50%, -50%)",
