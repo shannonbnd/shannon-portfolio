aa15076a9c34cf6117a9aafe601a70d69e859034
shannonbnd <bundhoos29@gmail.com>
2026-03-25 22:56:08 +0100

    Update Appareil3D.tsx

--- src/app/Appareil3D.tsx
+++ src/app/Appareil3D.tsx
@@ -7,7 +7,7 @@ const DESKTOP_CAMERA_ORBIT = "-5deg 82deg -7m";
 const MOBILE_CAMERA_ORBIT = "-5deg 82deg -6m";
 const DESKTOP_CAMERA_TARGET = "-0.4m 3m 0m";
 const MOBILE_CAMERA_TARGET = "-0.4m 0m 0m";
-const BASE_FOV = "-50deg";
+const BASE_FOV = "10deg";
 
 type Appareil3DProps = {
   viewerRef: RefObject<any>;
