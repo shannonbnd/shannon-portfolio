37d430e7dbd8cfb114ceb51ba845b0b8f33acf02
shannonbnd <bundhoos29@gmail.com>
2026-03-25 22:39:47 +0100

    Update Home.tsx

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -10,10 +10,10 @@ import Appareil3D from "../Appareil3D";
 
 type ProjectTicket = (typeof PROJECTS)[number];
 
-const BASE_CAMERA_ORBIT = "-5deg 82deg -9m";
+const BASE_CAMERA_ORBIT = "-5deg 82deg -18m";
 const MOBILE_CAMERA_ORBIT = "-5deg 82deg -6m";
 const BASE_CAMERA_TARGET = "-0.4m 0m 0m";
-const BASE_FOV = "46deg";
+const BASE_FOV = "70deg";
 
 export default function Home() {
   const navigate = useNavigate();
