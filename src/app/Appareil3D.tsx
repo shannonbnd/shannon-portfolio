69a1b05c7496f68288879482bf226dd5da2d6737
shannonbnd <bundhoos29@gmail.com>
2026-03-25 20:31:32 +0100

    Update DESKTOP_CAMERA_ORBIT distance in Appareil3D

--- src/app/Appareil3D.tsx
+++ src/app/Appareil3D.tsx
@@ -3,7 +3,7 @@ import { PROJECTS } from "../data/projects";
 
 type ProjectTicket = (typeof PROJECTS)[number];
 
-const DESKTOP_CAMERA_ORBIT = "-5deg 82deg -4.1m";
+const DESKTOP_CAMERA_ORBIT = "-5deg 82deg -7m";
 const MOBILE_CAMERA_ORBIT = "-5deg 82deg -5m";
 const BASE_CAMERA_TARGET = "-0.4m 0m 0m";
 const BASE_FOV = "40deg";
