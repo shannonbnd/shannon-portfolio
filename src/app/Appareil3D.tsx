8e0c3aa2afd67456275f7e6532cfdd1dd5ae0f86
shannonbnd <bundhoos29@gmail.com>
2026-03-25 22:44:47 +0100

    Change BASE_FOV from 24deg to 95deg

--- src/app/Appareil3D.tsx
+++ src/app/Appareil3D.tsx
@@ -7,7 +7,7 @@ const DESKTOP_CAMERA_ORBIT = "-5deg 82deg -2.5m";
 const MOBILE_CAMERA_ORBIT = "-5deg 82deg -6m";
 const DESKTOP_CAMERA_TARGET = "-0.4m 3m 0m";
 const MOBILE_CAMERA_TARGET = "-0.4m 0m 0m";
-const BASE_FOV = "24deg";
+const BASE_FOV = "95deg";
 
 type Appareil3DProps = {
   viewerRef: RefObject<any>;
