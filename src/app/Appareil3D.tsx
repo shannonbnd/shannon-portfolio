727d2fc0c594abcf2239e88fd64eb3cea4a28267
shannonbnd <bundhoos29@gmail.com>
2026-03-25 21:21:01 +0100

    Update DESKTOP_CAMERA_ORBIT to use 105% distance

--- src/app/Appareil3D.tsx
+++ src/app/Appareil3D.tsx
@@ -3,7 +3,7 @@ import { PROJECTS } from "../data/projects";
 
 type ProjectTicket = (typeof PROJECTS)[number];
 
-const DESKTOP_CAMERA_ORBIT = "-5deg 82deg 20m";
+const DESKTOP_CAMERA_ORBIT = "-5deg 82deg 105%";
 const MOBILE_CAMERA_ORBIT = "-5deg 82deg -5m";
 const DESKTOP_CAMERA_TARGET = "-0.4m 3m 0m";
 const MOBILE_CAMERA_TARGET = "-0.4m 0m 0m";
