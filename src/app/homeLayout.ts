63571c150695052683fb0ef5bcc1465282918dd3
Shannon Bundhoo <bundhoos29@gmail.com>
2026-04-15 11:11:27 +0200

    Reposition mobile controls and nudge desktop 3D

--- src/app/homeLayout.ts
+++ src/app/homeLayout.ts
@@ -1,10 +1,10 @@
 export const HOME_LAYOUT = {
   toggle: "left-[29px] top-[18px] lg:left-[-500px] lg:top-[34px]",
-  share: "left-[261px] top-[10px] h-[43px] w-[43px] lg:left-auto lg:right-[-384px] lg:top-[32px]",
-  cv: "left-[315px] top-[10px] h-[39px] w-[42px] lg:left-auto lg:right-[-442px] lg:top-[34px]",
-  gallery: "left-[369px] top-[10px] h-[40px] w-[42px] lg:left-auto lg:right-[-500px] lg:top-[34px]",
-  photo: "left-[63px] top-[620px] h-[98.625px] w-[309.75px] lg:top-[670px]",
-  email: "left-[93px] top-[730px] h-[57.375px] w-[250.875px] lg:top-[765px]",
+  share: "left-[261px] top-[-8px] h-[43px] w-[43px] lg:left-auto lg:right-[-384px] lg:top-[32px]",
+  cv: "left-[315px] top-[-8px] h-[39px] w-[42px] lg:left-auto lg:right-[-442px] lg:top-[34px]",
+  gallery: "left-[369px] top-[-8px] h-[40px] w-[42px] lg:left-auto lg:right-[-500px] lg:top-[34px]",
+  photo: "left-[63px] top-[600px] h-[98.625px] w-[309.75px] lg:top-[670px]",
+  email: "left-[93px] top-[705px] h-[57.375px] w-[250.875px] lg:top-[765px]",
 
    device:
      "left-1/2 top-1/2 h-[420px] w-[540px] -translate-x-1/2 -translate-y-1/2",
